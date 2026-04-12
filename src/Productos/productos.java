9d5a9285105446132df3e86c9ffd58558cd1d096
JESUS AXEL AVILA GARCIA <axeljesus43@outlook.com>
2026-04-12 02:14:00 -0600

    creamos el repositorio

---
+++ src/Productos/productos.java
@@ -0,0 +1,10 @@
+package Productos;
+
+public class productos {
+
+private int idProducto;
+private String nombreProducto;
+private float precioProducto;
+
+
+}
---
+++ src/calculoyprecios/calculo.java
@@ -0,0 +1,4 @@
+package calculoyprecios;
+
+public class calculo {
+}
